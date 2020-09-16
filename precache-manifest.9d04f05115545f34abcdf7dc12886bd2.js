a6ad212a2fc408f104a4f9f219ea7cbcf467d403
javeoff <62588539+javeoff@users.noreply.github.com>
2020-09-16 17:13:07 +0300

    Add files via upload

---
+++ precache-manifest.9d04f05115545f34abcdf7dc12886bd2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "628b4df1bf98786dfea434e01020f333",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8015b5f4418342432b8a",
+    "url": "/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "5a2755944d325209b104",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "8015b5f4418342432b8a",
+    "url": "/static/js/2.a901fa5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a901fa5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2755944d325209b104",
+    "url": "/static/js/main.73eb747a.chunk.js"
+  },
+  {
+    "revision": "bde3be86d147a096631f",
+    "url": "/static/js/runtime-main.944b771c.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
